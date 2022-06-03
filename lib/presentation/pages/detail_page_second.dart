fc722c627b54e0774b61a281e507a5d0923732a5
zamama-zaman <zamamazaman@gmail.com>
2022-06-03 21:21:28 +0500

    Updated Code

---
+++ lib/presentation/pages/detail_page_second.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class DetailPageSecond extends StatelessWidget {
+  const DetailPageSecond({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("I Will Rule The World!"),
+      ),
+    );
+  }
+}
